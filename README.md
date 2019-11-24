3c9c8eb1c7240182fd1029d88a90fc7703996a7e
krist yohan <massashok420@gmail.com>
2019-11-24 19:32:26 +0530

    hello ashok

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 Create new file `touch basics.js`
 
 How to run javascript file on ubuntu command prompt `node filename`
+
+#### Example: `node basics.js`
--- basics.js
+++ basics.js
@@ -2,8 +2,21 @@
 
 // ++ Statement || Programming Instructions
 
-var x, y, z;
-x = 5;
-y = 4;
-z = x + y;
+var x, y, z; // Statement 1
+x = 5; // Statement 2
+y = 4; // Statement 3
+z = x + y; // Statement 4
 console.log('The value of z is ' + z + '.');
+// ; Separates statements
+
+sa = 5;
+sb = 6;
+sc = sa + sb; // Multiple statements
+console.log(sc);
+
+var ws = 'CP'; //Ignores Multiple white spaces
+// {} Code Block
+function codeBlock() {
+  console.log('Hello khrish');
+}
+codeBlock();
