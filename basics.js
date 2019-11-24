fc63f5a19e062e08f051c48f50b7a7c8572b44be
krist yohan <massashok420@gmail.com>
2019-11-24 19:51:01 +0530

    arithmatic added

--- basics.js
+++ basics.js
@@ -20,3 +20,50 @@ function codeBlock() {
   console.log('Hello khrish');
 }
 codeBlock();
+
+// Syntax
+var x, y, z; // How to declare values
+x = 5;
+y = 6; // How to assign values
+z = x + y; // How to compute values
+console.log(z); // print value in command prompt
+
+// ++ Variables || var creates variables
+var price1 = 5;
+var price2 = 2;
+var sub = price1 - price2;
+console.log('The sub value is: ' + sub);
+
+// ++Case Sensitive
+var lastName, lastname;
+lastName = 'khrist';
+lastname = 'yohan';
+console.log(lastName + ' ' + lastname);
+
+// ++ Operators
+// substraction
+var a = 4;
+var b = 3;
+var c = a - b;
+//multiplication
+var d = 5;
+var e = 4;
+var f = d * e;
+//division
+g = 10;
+h = 2;
+i = g / 2;
+// Modulo
+j = 10;
+k = 4;
+l = j % k;
+
+console.log(c, f, i, l);
+
+// arithmatic
+a = 5;
+b = 10;
+x = ((10 - 5) * a) / b; // x = 2.5
+console.log(x);
+x = ((100 - 50) * a) / b; // x = 25
+console.log(x);
