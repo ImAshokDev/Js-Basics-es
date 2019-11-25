fbb763ac9f34beaf51d65ee7c5d99ca82263c118
krist yohan <massashok420@gmail.com>
2019-11-25 13:37:12 +0530

    switch  statement added

--- basics.js
+++ basics.js
@@ -348,3 +348,30 @@ function ElseIfFunc() {
   console.log(greeting);
 }
 ElseIfFunc();
+
+//Switch
+
+var day; //Use the switch statement to select one of many code blocks to be executed.
+switch (new Date().getDay()) {
+  case 0:
+    day = 'sunday';
+    break;
+  case 1:
+    day = 'Monday';
+    break;
+  case 2:
+    day = 'Tuesday';
+    break;
+  case 3:
+    day = 'Wednesday';
+    break;
+  case 4:
+    day = 'Thursday';
+    break;
+  case 5:
+    day = 'Friday';
+    break;
+  case 6:
+    day = 'Saturday';
+}
+console.log(' Today is : ' + day);
