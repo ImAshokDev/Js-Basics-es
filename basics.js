b67582799f2457737b57bf35ab392ce1256f7655
krist yohan <massashok420@gmail.com>
2019-11-25 17:03:57 +0530

    break and continue

--- basics.js
+++ basics.js
@@ -474,3 +474,27 @@ while (cars[i]) {
   i++;
 }
 console.log(text);
+
+// Break and Continue
+
+//Break Statement
+var text = ''; //The break statement can also be used to jump out of a loop.
+var i;
+for (i = 0; i < 10; i++) {
+  if (i === 3) {
+    break;
+  }
+  text += 'The number is: ' + i + '\n';
+}
+console.log(text);
+
+//continue Statement
+var text = ''; //The break statement breaks the loop and continues executing the code after the loop .
+var i;
+for (i = 0; i < 10; i++) {
+  if (i === 3) {
+    continue;
+  }
+  text += 'The number is: ' + i + '\n';
+}
+console.log(text);
