19fbd0590d39ab50f3c3d2a1080f15c1e38bd5cd
krist yohan <massashok420@gmail.com>
2019-11-28 13:11:00 +0530

    PADSTART AND PADEND added in es8

---
+++ es8_2018.js
@@ -0,0 +1,9 @@
+// PADsTART ():
+
+let str = 'a3'; //  (This method pads a string with another string at the beginning.)
+console.log(str.padStart(5, '#'));
+
+// PENEND();
+let one = 'Bat'; //(This method pads a string with another string and makes the resulting string reach a given length.
+//It adds spaces at the end of the string.)
+console.log(one.padEnd(8, '.'));
