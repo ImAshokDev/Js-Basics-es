dff4a98432d4319f70e83f81597f522df30710c5
krist yohan <massashok420@gmail.com>
2019-11-28 11:32:26 +0530

    es5 object added

--- es5_2009.js
+++ es5_2009.js
@@ -100,3 +100,101 @@ var date = Date.now();
 console.log(date);
 var currentDate = new Date();
 console.log(currentDate);
+
+// ++ Property Getters and Setters
+// Getting and Setting the values to the object
+var person = {
+  firstname: 'kheist',
+  lastname: 'yohan',
+  game: '',
+
+  get fullname() {
+    return this.firstname + ' ' + this.lastname;
+  },
+  set favGame(val) {
+    this.game = val;
+  }
+};
+
+console.log(person);
+console.log(person.fullname);
+console.log(person.game);
+console.log((person.favGame = 'footvall'));
+console.log(person);
+
+// ++ New Object Property Methods
+// Lets the user define the property of an object and/or change its value
+// Object
+var people = {
+  firstName: 'khrist',
+  lastName: 'yohan',
+  favGame: ''
+};
+// Change a Property
+Object.defineProperty(people, 'favGame', {
+  value: 'FootBall',
+  writable: true,
+  enumerable: false,
+  configurable: true
+});
+var txt = '';
+for (var x in people) {
+  txt += people[x] + '\n';
+}
+console.log(txt);
+// We are setting, but can't be changed
+people.favGame = 'cricket';
+console.log(people);
+
+// var pplarr = {
+//     firstName: "chaal",
+//     lastName: "pritam",
+//     mygame: "NO"
+// };
+// Object.defineProperty(pplarr, "mygame", {
+//     get : function() {
+//         return mygame;
+//     },
+//     set : function(value) {
+//         mygame = value;
+//     }
+// });
+// // Change Fav Game
+// pplarr.mygame = "FootBall";
+// console.log(pplarr.mygame);
+
+// ++ Adding or changing an object property
+// Object.defineProperty(object, property, descriptor)
+
+// ++ Adding or changing many object properties
+// Object.defineProperties(object, descriptors)
+
+// ++ Accessing Properties
+// Object.getOwnPropertyDescriptor(object, property)
+
+// ++ Returns all properties as an array
+// Object.getOwnPropertyNames(object)
+
+// ++ Returns enumerable properties as an array
+// Object.keys(object)
+
+// ++ Accessing the prototype
+// Object.getPrototypeOf(object)
+
+// ++ Prevents adding properties to an object
+// Object.preventExtensions(object)
+
+// ++ Returns true if properties can be added to an object
+// Object.isExtensible(object)
+
+// ++ Prevents changes of object properties (not values)
+// Object.seal(object)
+
+// ++ Returns true if object is sealed
+// Object.isSealed(object)
+
+// ++ Prevents any changes to an object
+// Object.freeze(object)
+
+// ++ Returns true if object is frozen
+// Object.isFrozen(object)
