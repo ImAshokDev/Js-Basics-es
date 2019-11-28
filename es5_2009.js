fa232c8b25476d73ef71e744ae69813203da6976
krist yohan <massashok420@gmail.com>
2019-11-28 09:59:31 +0530

    es5 string added

---
+++ es5_2009.js
@@ -0,0 +1,13 @@
+'use strict';
+// ++ "use strict" Directive
+// for proper js and error reporting
+
+var st;
+
+st = 22; // v is not defined
+console.log(st);
+
+// ++ String.trim()
+
+var tri = '   khrist yohan    ';
+console.log(tri);
