09a865c9f9081a7d9d912cebbb5d5f588b6d6a9e
krist yohan <massashok420@gmail.com>
2019-11-28 11:57:14 +0530

    const added in es6

--- es6_2015.js
+++ es6_2015.js
@@ -7,3 +7,13 @@ console.log(l); // Here l is 10
   console.log(l);
 }
 console.log(l); // Here x is 10
+
+// ++ JavaScript const
+// Declaring constant values which cannot be changed
+const c = 89;
+console.log(c);
+{
+  const c = 10;
+  console.log(c);
+}
+console.log(c);
